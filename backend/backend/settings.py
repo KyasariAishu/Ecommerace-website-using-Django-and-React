8720097da44edf07d3280bd59e7082f685a666db
KyasariAishu <kyasari1106@gmail.com>
2025-06-13 08:21:05 +0530

    Data is being fetched from Backend instead of prodcut list

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 
 # Media files (uploaded by users)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
 from .products import products
+from .models import Product
+from .serailizers import ProductSerializer
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -25,13 +28,19 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products=Product.objects.all() # This data needs to be serialized before it is sent to frontend .
+    # it wraps our products which is a model data into a json format 
+    serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request,pk):
-    product = None
-    for i in products:
-        if i['_id']==pk:
-            product=i
-            break
-    return Response(product)
+    # product = None
+    # for i in products:
+    #     if i['_id']==pk:
+    #         product=i
+    #         break
+
+    prodcut= Product.objects.get(_id=pk)
+    serializer= ProductSerializer(prodcut,many=False)
+    return Response(serializer.data)
